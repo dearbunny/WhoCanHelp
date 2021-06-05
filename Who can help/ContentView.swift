a1e7012c3f0050a1ce844c676f2a61eebe448f92
rose16 <twno16@hotmail.com>
2021-06-05 22:56:57 +0800

    Initial Commit

---
+++ Who can help/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Who can help
+//
+//  Created by ROSE on 2021/6/5.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Who can help/Who_can_helpApp.swift
@@ -0,0 +1,17 @@
+//
+//  Who_can_helpApp.swift
+//  Who can help
+//
+//  Created by ROSE on 2021/6/5.
+//
+
+import SwiftUI
+
+@main
+struct Who_can_helpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
